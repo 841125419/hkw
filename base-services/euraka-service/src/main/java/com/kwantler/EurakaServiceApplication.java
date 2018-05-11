dfaeb8d0cfd7b07a7384629e0c95af2dc2a814aa
chenmingjian <841125419@qq.com>
2018-05-11 21:58:28 +0800

    commit db-center and gateway-service

--- base-services/euraka-service/src/main/java/com/kwantler/EurakaServiceApplication.java
+++ base-services/euraka-service/src/main/java/com/kwantler/EurakaServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurakaServiceApplication {
     public static void main(String[] args) {
+        //ddasdfasdf
         SpringApplication.run(EurakaServiceApplication.class,args);
     }
 }
---
+++ base-services/gateway-service/src/main/java/com/kwantler/GatewayServiceApplication.java
@@ -0,0 +1,26 @@
+package com.kwantler;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class,args);
+    }
+
+    @LoadBalanced
+    @Bean
+//    @Qualifier("no-oauth")
+    public RestTemplate loadBalancedRestTemplate() {
+        return new RestTemplate();
+    }
+}
